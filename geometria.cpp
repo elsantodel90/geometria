a74b0b5e7974fa13d88ca5f10c84550af7b76ecc
Agustin Santiago Gutierrez <elsantodel90@gmail.com>
2015-01-29 02:25:43 -0300

    Ahi logre que compile. Ejecutable al .gitignore

--- geometria.cpp
+++ geometria.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
+#define forn(i,n) for(int i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for(int i=(s);i<(int)(n);i++)
+
 // INICIO DE CODIGO DE GEOMETRIA
 
 typedef long long escalar; // Escalar, puede ser de punto flotante, o tambien entero cuando los puntos y operaciones que hagamos nos mantienen en enteros.
-typedef long double floating; // Se usa para marcar que estas operaciones si o si utilizan/devuelven numeros de punto flotante, no enteros.
+typedef double floating; // Se usa para marcar que estas operaciones si o si utilizan/devuelven numeros de punto flotante, no enteros.
 
 const escalar INF = 0x7FFFFFFFFFFFFFFFLL;
-const floating epsilon = 1e-9;
-#define feq(a,b) (fabs((a)-(b)) < epsilon) // Se puede cambiar (cuando aplica) por la igualdad exacta si se usan operaciones en enteros.
+const floating EPSILON = 1e-11;
+#define feq(a,b) (fabs((a)-(b)) < EPSILON) // Se puede cambiar (cuando aplica) por la igualdad exacta si se usan operaciones en enteros.
                                            // Asi como esta deberia producir codigo que funciona igualmente 
                                            // (aunque se pasa a float para hacer la cuenta con epsilon al pedo).
 struct pto{
@@ -24,17 +29,16 @@ struct pto{
     escalar operator^ (const pto& o) const { return x*o.y - y*o.x; }
     pto operator* (escalar k) const { return pto(k * x, k * y);}
     pto operator/ (floating k) const { return pto(x / k, y / k);}
-    escalar normaSqr() const { return (*this)*(*this);} // TODO: Chequear que esto no compile si nos comemos alguno(s) de los asteriscos.
+    escalar normaSqr() const { return x*x+y*y; }
     floating norma() const { return hypot(x,y);} // hypot hace la cuenta de la hipotenusa mejor de lo que uno la hace de la manera obvia.
     pto normal() const { return pto(-y, x); } // El vector girado 90 grados en sentido antihorario, asumiendo sistema de coordenadas usual
     pto unitario() const { return (*this) / norma(); } // Vector unitario en la direccion de p
-    pto proyectar(const pto &p) const { return ((p*d) / d.normaSqr()) * d; } // Proyecta sobre el nuestro
-    pto reflejar(const pto &p) const { return p - 2 * normal().proyectar(p); } // Refleja al otro vector usandonos como eje de simetria
-    pto simetrico(const pto &p) const { return 2 * (*this) - p; } // Simetrico de p con respecto a o
-    pto rotar(const pto &o, const pto &a) const { //rota p con centro en o para que a quede horizontal (hacia la derecha)
-        linea l = linea::por(o,a);
-        linea n = l.normalPor(o);
-        return o + pto(-n.distConSigno(*this) , l.distConSigno(*this));
+    pto proyectar(const pto &p) const { return *this * ((p * *this) / normaSqr()); } // Proyecta sobre el nuestro
+    pto reflejar(const pto &p) const { return p - normal().proyectar(p) * 2; } // Refleja al otro vector usandonos como eje de simetria
+    pto simetrico(const pto &p) const { return *this * 2 - p; } // Simetrico de p con respecto a o
+    pto rotar(const pto &o, const pto &a) const { //Nos rota con centro en o para que a quede horizontal (hacia la derecha)
+        pto vx = a-o, vy = vx.normal();
+        return o + pto((*this - o) * vx / vx.norma() , (*this - o) * vy / vy.norma());
     }
     pto rotarAngulo(floating angle) const { // En sentido antihorario
         floating c = cos(angle), s = sin(angle);
@@ -48,7 +52,8 @@ struct pto{
         return alpha;
     }
 };
-pto operator* (escalar a, const pto &p) { return p * k; }
+
+pto operator* (escalar k, const pto &p) { return p * k; }
 escalar distSqr(const pto &a, const pto &b) { return (a-b).normaSqr();}
 floating dist(const pto &a, const pto &b) { return (a-b).norma();}
 
@@ -60,8 +65,8 @@ struct linea{
     linea normalPor(const pto &p) const { return linea(p, d.normal()); }
     linea paralelaPor(const pto &p) const { return linea(p, d); }
     bool contiene(const pto &p) const { return feq(d ^ (p-s),0); }
-    floating dist(const pto &p) const { return fabs(distConSigno()); }
     floating distConSigno(const pto &p) const { return (d ^ (p-s)) / d.norma(); }
+    floating dist(const pto &p) const { return fabs(distConSigno(p)); }
     pto proyeccion(const pto &p) const { return s + d.proyectar(p-s); }
     pto reflejo(const pto &p) const { return s + d.reflejar(p-s); }
 };
@@ -83,32 +88,27 @@ struct segmento{
     pto f, t;
     segmento() {}
     segmento(const pto &a, const pto &b) : f(a), t(b) {}
-    bool contiene(const pto &p) const { return feq((p-f)^(t-f),0) && (p-f)*(p-t)<=epsilon; }
-    floating longitud() const { return dist(f,t); }
+    bool contiene(const pto &p) const { return feq((p-f)^(t-f),0) && (p-f)*(p-t)<=EPSILON; }
+    floating longitud() const { return ::dist(f,t); }
     linea recta() const { return linea::por(f,t); }
     linea mediatriz() const { return recta().normalPor(0.5 * (f+t)); }
     floating dist(const pto &p) const {
-        floating h = recta().dist(p), c = hypot(longitud(), h);
-        floating d1 = dist(f, p), d2 = dist(t, p);
+        floating h = recta().dist(p), b = longitud(), c = hypot(b, h);
+        floating d1 = ::dist(f, p), d2 = ::dist(t, p);
         if(b<EPSILON || c <= d1 || c <= d2)return min(d1, d2); else return h;
     }
 
 };
 
-enum StatusLineaSegmento {NADA, PARALELO, SE_CORTAN, CONTENIDO};
+enum StatusLineaSegmento {SIN_RELACION, PARALELO, LA_CORTA, CONTENIDO};
 
 StatusLineaSegmento status(const linea &l, const segmento &s) {
     linea r = s.recta(); StatusRectas stat = status(l, r);
     if (stat == PARALELAS) return PARALELO;
     if (stat == COINCIDENTES) return CONTENIDO;
-    if (s.contiene(interseccion(r, l))) return SE_CORTAN;
-    return NADA;
+    if (s.contiene(interseccion(r, l))) return LA_CORTA;
+    return SIN_RELACION;
 }
-
-tdbl dist(seg a, seg b){
-	return (inter(a, b))?0.0:min(min(dist(a.f, b), dist(a.s, b)), min(dist(b.f, a), dist(b.s, a)));
-}
-
 
 int sgn(escalar a){ if(feq(a,0)) return 0; return (a>0)? 1:-1; }
 
@@ -129,19 +129,22 @@ enum StatusSegmentos {NADA, PARALELOS, ALINEADOS, SE_CORTAN, SOLAPADOS};
 
 StatusSegmentos status(const segmento &s1, const segmento &s2){
     linea l1 = s1.recta(), l2 = s2.recta();
-    StatusRectas status = status(l1,l2);
-    if (status == PARALELAS) return PARALELOS;
-    if (status == SECANTES)
+    StatusRectas stat = status(l1,l2);
+    if (stat == PARALELAS) return PARALELOS;
+    if (stat == SECANTES)
     {
         pto inter = interseccion(l1,l2);
         return (s1.contiene(inter) && s2.contiene(inter)) ? SE_CORTAN : NADA;
     }
-    floating f = (s2.f-s1.f)*(s1.t-s1.f)/norma2(s1.t-s1.f);
-    floating t = (s2.t-s1.f)*(s1.t-s1.f)/norma2(s1.t-s1.f);
-    if(f > t) swap(f,t);
-    floating dd = min(t,(tipo)1.0) - max(f,(tipo)0.0);
+    pto u = s1.t-s1.f; floating f = (s2.f-s1.f)*u/u.normaSqr(), t = (s2.t-s1.f)*u/u.normaSqr();
+    if(f > t) swap(f,t); floating dd = min(t,1.0) - max(f,0.0);
     return feq(dd,0)? SE_CORTAN : (dd>0)? SOLAPADOS : ALINEADOS;
 }
+
+floating dist(const segmento &a, const segmento &b){
+	return (hayInterseccion(a, b))?0.0:min(min(b.dist(a.f), b.dist(a.t)), min(a.dist(b.f), a.dist(b.t)));
+}
+
 
 linea bisectriz(const pto &o,const pto &b,const pto &c) { return linea(o, dist(o,c)*(b-o)+dist(o,b)*(c-o)); }
 
@@ -153,7 +156,7 @@ struct triangulo
     pto circuncentro() const { return interseccion(segmento(a,b).mediatriz(), segmento(a,c).mediatriz()); }
     floating circunradio() const { return dist(a, circuncentro()); }
     pto ortocentro() const { return interseccion(linea::por(a, linea::por(b,c).proyeccion(a)),
-                                                 linea::por(b, linea::por(a,c).proyeccion(b))) }
+                                                 linea::por(b, linea::por(a,c).proyeccion(b))); }
     pto baricentro() const { return (a+b+c) / 3.0; }
     pto incentro() const { return interseccion(bisectriz(a,b,c), bisectriz(b,a, c)); }
     floating inradio() const { return 2 * area() / perimetro(); } // O tambien, linea::por(a,b).dist(incentro())
@@ -163,14 +166,17 @@ struct triangulo
 
 enum StatusLineaCirculo {EXTERIOR, TANGENTE, SECANTE};
 
+struct circulo;
+pair<pto,pto> interseccion(const circulo &c1, const circulo &c2);
+
 struct circulo{
     pto c; floating r;
     circulo() {}
     circulo(const pto &cc, floating rr) : c(cc), r(rr) {}
     StatusLineaCirculo status(const linea &l) const {
         floating dd = l.dist(c);
-        if(feq(dd,c.r)) return TANGENTE;
-        return (dd<c.r)? SECANTE:EXTERIOR;
+        if(feq(dd,r)) return TANGENTE;
+        return (dd<r)? SECANTE:EXTERIOR;
     }
     pair<pto,pto> interseccion(const linea &l) const { // Asume que la recta no es exterior
         pto p = l.proyeccion(c);
@@ -179,14 +185,14 @@ struct circulo{
         return make_pair(p+dif, p-dif);
     }
     pair<pto,pto> tangencias(const pto &p) const { // Asume que el punto es exterior o tangente al circulo
-        floating dd = sqrt(distSqr(p,c.c)-c.r*c.r);
-        return interseccion(circulo(p, dd),c);
+        floating dd = sqrt(distSqr(p,c)-r*r);
+        return ::interseccion(circulo(p, dd),*this);
     }
 };
 
-enum StatusCirculos {EXTERIORES, TANGENCIA_EXTERIOR, SECANTES, TANGENCIA_INTERIOR, CONTENIDA, CONCENTRICAS};
+enum StatusCirculos {EXTERIORES, TANGENCIA_EXTERIOR, DOS_INTERSECCIONES, TANGENCIA_INTERIOR, CONTENIDA, CONCENTRICAS};
 
-int status(const circulo &c1, const circulo &c2){
+StatusCirculos status(const circulo &c1, const circulo &c2){
     floating dd = dist(c1.c, c2.c);
     if(feq(dd,0.0)) return CONCENTRICAS;
     floating R = max(c1.r,c2.r), r = min(c1.r,c2.r);
@@ -194,7 +200,7 @@ int status(const circulo &c1, const circulo &c2){
     if(dd > R+r) return EXTERIORES;
     if(feq(dd+r,R)) return TANGENCIA_INTERIOR;
     if((dd + r < R)) return CONTENIDA;
-    return SECANTES;
+    return DOS_INTERSECCIONES;
 }
 
 pair<pto,pto> interseccion(const circulo &c1, const circulo &c2){ // Asume que son tangentes o secantes
@@ -203,7 +209,7 @@ pair<pto,pto> interseccion(const circulo &c1, const circulo &c2){ // Asume que s
     return c1.interseccion(linea::por(c1.c, c2.c).normalPor(c1.c + mult*(c2.c-c1.c)));
 }
 
-enum StatusPuntoPoligono {EXTERIOR, FRONTERA, INTERIOR};
+enum StatusPuntoPoligono {PUNTO_EXTERIOR, EN_FRONTERA, PUNTO_INTERIOR};
 
 struct poligono
 {
@@ -212,7 +218,7 @@ struct poligono
     poligono() : n(0) {}
     poligono(const vector<pto> &vv) : v(vv), n(vv.size()) {}
     void push(const pto &p) { v.push_back(p); n++; }
-    void pop(const pto &p) { v.pop_back(); n--; }
+    void pop() { v.pop_back(); n--; }
     escalar areaPor2ConSigno() const {
         escalar res = 0;
         for(int i = 0, j = n-1; i < n; j = i++) res += v[j]^v[i];
@@ -234,7 +240,7 @@ struct poligono
         floating res = HUGE_VAL;
         for(int b = 0, a = n-1, cand = 1, far = 0; b < n; a = b++) {
             while(((v[b]-v[a])^(v[cand]-v[far])) >= 0) {far = cand++; cand %= n;}
-            res = min(res, linea::por(v[a], v[b]).dist(pol[far]));
+            res = min(res, linea::por(v[a], v[b]).dist(v[far]));
         }
         return res;
     }
@@ -262,13 +268,13 @@ struct poligono
     StatusPuntoPoligono status(const pto &p) const { // Esta escrito para coordenadas enteras, con floats habria que usar comparaciones estandar con EPS
         unsigned i, j, m, M, c = 0;
         for(i = 0, j = v.size()-1; i < v.size(); j = i++){
-            if(feq(dist(p,v[i])+dist(p,v[j]),dist(v[i],v[j]))) return FRONTERA; // Frontera debe manejarse aparte de esta forma
+            if(feq(dist(p,v[i])+dist(p,v[j]),dist(v[i],v[j]))) return EN_FRONTERA; // Frontera debe manejarse aparte de esta forma
             if((v[i].y <= p.y && p.y < v[j].y)||(v[j].y <= p.y && p.y < v[i].y)){
                 m = i; M = j; if(v[i].y > v[j].y) swap(m,M);
                 c ^= (((v[m]-p)^(v[M]-p)) > 0);
             }
         }
-        return c ? INTERIOR : EXTERIOR;
+        return c ? PUNTO_INTERIOR : PUNTO_EXTERIOR;
     }
 };
 
@@ -279,14 +285,14 @@ bool menorEnYluegoEnX(const pto &p1, const pto &p2){
   return (p1.y==p2.y)?(p1.x<p2.x):(p1.y<p2.y);
 }
 bool menorGrahamScan(const pto &p1,const pto &p2){
-  tipo ar = (p1-r)^(p2-r);
-  return(ar==0)?(dist2(p1,r)<dist2(p2,r)):ar>0;
+  escalar ar = (p1-chull_r)^(p2-chull_r);
+  return(ar==0)?(distSqr(p1,chull_r)<distSqr(p2,chull_r)):ar>0;
 }
 poligono chull(vector<pto> v) { // Devuelve la chull, sin puntos alineados, en sentido antihorario. Asume puntos distintos.
-  if(v.size()<3) return pol;
+  if(v.size()<3) return v;
   chull_r = *(min_element(v.begin(),v.end(),menorEnYluegoEnX));
   sort(v.begin(),v.end(), menorGrahamScan);
-  int i=0, s; poligono ch;
+  int i=0; poligono ch;
   while(i<(int)v.size()){
     if(ch.n>1 && ((ch.v[ch.n-1]-ch.v[ch.n-2])^(v[i]-ch.v[ch.n-2]))<=0) ch.pop();
     else ch.push(v[i++]);
@@ -300,26 +306,27 @@ poligono chull(vector<pto> v) { // Devuelve la chull, sin puntos alineados, en s
 ord(cp_menorEnX,x,y) ord(cp_menorEnY,y,x)
 
 const int CLOSEST_PAIR_MAX_N = 110000;
-pto v [CLOSEST_PAIR_MAX_N]; // INPUT
+pto closest_pair_v[CLOSEST_PAIR_MAX_N]; // INPUT
+#define v closest_pair_v
 struct ClosestPair {
     pto a,b; escalar d;
     ClosestPair() : d(INF) {}
     ClosestPair(const pto &p1, const pto &p2) : a(p1), b(p2), d(distSqr(p1,p2)) {}
-    void check(const pto &p1, const pto &p1) {
+    void check(const pto &p1, const pto &p2) {
         escalar newD = distSqr(p1, p2);
         if (newD < d) { d = newD; a = p1; b = p2; }
     }
-}
+};
 ClosestPair cpair(int ini, int fin){
   if(fin-ini <= 16) // Tuneable: como mucho impone un costo total N * 16 para los casos base
   {
       ClosestPair ret;
-      forsn(i, ini, fin) forsn(j, ini, i) ret.check(vx[i], vx[j]);
+      forsn(i, ini, fin) forsn(j, ini, i) ret.check(v[i], v[j]);
       sort(v+ini, v+fin, cp_menorEnY);
       return ret;
   }
   int m = (ini+fin)/2; escalar lineX = v[m].x;
-  ClosestPair res = cpair(ini,m); right = cpair(m,fin);
+  ClosestPair res = cpair(ini,m), right = cpair(m,fin);
   res.check(right.a, right.b);
   static pto vy[CLOSEST_PAIR_MAX_N];
   merge(v+ini, v+m, v+m, v+fin, vy + ini, cp_menorEnY);
@@ -336,10 +343,10 @@ ClosestPair cpair(int ini, int fin){
 }
 ClosestPair closest_pair(int N){ // Asume que hay al menos dos puntos. Hace aritmetica con enteros si son puntos enteros.
   sort(v, v+N,cp_menorEnX);
-  forsn(i, 1, N) if(v[i].x==v[i-1].x && v[i].y==v[i-1].y) return 0;
+  forsn(i, 1, N) if(v[i].x==v[i-1].x && v[i].y==v[i-1].y) return ClosestPair(v[i], v[i-1]);
   return cpair(0,N);
 }
-
+#undef v
 
 // MINIMUM BOUNDING CIRCLE. Tiempo lineal esperado.
 
@@ -350,7 +357,7 @@ circulo mdisk_deIni(vector<pto> v){ //v.size()<=3
     case 2: return circulo(0.5 * (v[0] + v[1]) , 0.5 * dist(v[0], v[1])); break;
     default:
         triangulo tri(v[0], v[1], v[2]);
-        if(feq(tri.area(), 0)) {assert(false); swap(v[1],v[2]); v.pop_back(); return mdisk_deIni(v);} // Nunca deberia pasar esto... PPP lo tenia asi.
+        if(feq(tri.area(), 0)) {swap(v[1],v[2]); v.pop_back(); return mdisk_deIni(v);} // Nunca deberia pasar esto... PPP lo tenia asi.
         pto c = tri.circuncentro(); return circulo(c, dist(v[0],c));
   }
 }
@@ -379,5 +386,6 @@ circulo mdisk_minDisc(vector<pto> ps){ // ESTA ES LA QUE SE USA. Asume puntos di
 
 int main()
 {
+    
     return 0;
 }
